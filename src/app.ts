b7e511b4700a375e3983efe935ab4a4c8abc16b0
Taofik Adesina <91717420+Kunlexi@users.noreply.github.com>
2023-10-13 08:22:09 +0100

    Update2

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,8 @@ const corsOrigin = {
   optionSuccessStatus: 200,
 };
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
+// app.use(cors({ origin: "*" }));
 // app.use(cors(corsOrigin));
 
 app.use(express.urlencoded({ extended: true }));
